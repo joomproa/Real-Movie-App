f161cdebd26a2b89174c4372d67892f4cbb195c8
Akibu Ismail <akibu.ismail@yahoo.com>
2023-04-18 23:35:26 -0700

    Port template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { MovieComponent } from './pages/movie/movie.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'home', component: HomeComponent},
+  { path: 'movie', component: MovieComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
